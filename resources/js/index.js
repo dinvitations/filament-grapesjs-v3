6cbd8685bf2f2c2407af52a7e43aea64b340c97a
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-06-06 16:48:56 +0700

    fix: loop on loadProjectData

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,15 +1,18 @@
+let grapesInstance = null;
+
 document.addEventListener('alpine:init', () => {
     Alpine.data(
         "grapesjs",
         ({ state, statePath, readOnly, tools, minHeight, container, plugins, settings }) => ({
             instance: null,
-            state: state,
-            tools: tools,
-            plugins: plugins,
-            settings: settings,
+            state,
+            tools,
+            plugins,
+            settings,
+
             init() {
                 let enabledTools = {};
-
+                
                 let allSettings = {
                     height: minHeight + 'px',
                     container: container ? container : ".filament-grapesjs .grapesjs-wrapper",
@@ -19,38 +22,41 @@ document.addEventListener('alpine:init', () => {
                     storageManager: false,
                     plugins: plugins,
                     ...settings
-                }
-                this.instance =  grapesjs.init( allSettings );
-                this.instance.on('load', e => {
+                };
+
+                grapesInstance = grapesjs.init(allSettings);
+
+                this.instance = grapesInstance;
+
+                grapesInstance.on('load', () => {
                     if (isNotEmpty(this.state?.grapesjs?.projectData)) {
-                        this.instance.loadProjectData(JSON.parse(this.state.grapesjs.projectData));
+                        grapesInstance.loadProjectData(JSON.parse(this.state.grapesjs.projectData));
                     } else {
                         if (isNotEmpty(this.state?.grapesjs?.components)) {
-                            this.instance.setComponents(JSON.parse(this.state.grapesjs.components));
+                            grapesInstance.setComponents(JSON.parse(this.state.grapesjs.components));
                         } else if (isNotEmpty(this.state?.html)) {
-                            this.instance.setComponents(this.state.html);
+                            grapesInstance.setComponents(this.state.html);
                         }
 
                         if (isNotEmpty(this.state?.grapesjs?.style)) {
-                            this.instance.setStyle(JSON.parse(this.state.grapesjs.style));
+                            grapesInstance.setStyle(JSON.parse(this.state.grapesjs.style));
                         } else if (isNotEmpty(this.state?.css)) {
-                            this.instance.setStyle(this.state.css);
+                            grapesInstance.setStyle(this.state.css);
                         }
                     }
                 });
-                this.instance.on('update', e => {
-                    var projectData = this.instance.getProjectData();
-                    var components = this.instance.getComponents();
-                    var styles = this.instance.getStyle();
 
-                    var rawHtml = this.instance.getHtml({
-                        cleanId: true
-                    });
-                    var cssContent = this.instance.getCss();
-                    var jsContent = this.instance.getJs();
+                grapesInstance.on('update', () => {
+                    const projectData = grapesInstance.getProjectData();
+                    const components = grapesInstance.getComponents();
+                    const styles = grapesInstance.getStyle();
 
-                    var extract = rawHtml.match(/<body\b[^>]*>([\s\S]*?)<\/body>/);
-                    var htmlContent = extract ? extract[1] : rawHtml;
+                    const rawHtml = grapesInstance.getHtml({ cleanId: true });
+                    const cssContent = grapesInstance.getCss();
+                    const jsContent = grapesInstance.getJs();
+
+                    const extract = rawHtml.match(/<body\b[^>]*>([\s\S]*?)<\/body>/);
+                    const htmlContent = extract ? extract[1] : rawHtml;
 
                     this.state = {
                         grapesjs: {
@@ -62,26 +68,18 @@ document.addEventListener('alpine:init', () => {
                         css: cssContent,
                         js: jsContent
                     };
-                })
+                });
             }
         })
-    )
-})
+    );
+});
 
 function isNotEmpty(val) {
     if (val === null || val === undefined) return false;
 
-    if (Array.isArray(val)) {
-        return val.length > 0;
-    }
-
-    if (typeof val === 'object') {
-        return Object.keys(val).length > 0;
-    }
-
-    if (typeof val === 'string') {
-        return val.trim() !== '' && val.trim() !== '0';
-    }
+    if (Array.isArray(val)) return val.length > 0;
+    if (typeof val === 'object') return Object.keys(val).length > 0;
+    if (typeof val === 'string') return val.trim() !== '' && val.trim() !== '0';
 
     return !!val;
 }
